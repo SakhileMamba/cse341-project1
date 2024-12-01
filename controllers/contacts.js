71f3ee909c4cae6acd4c806164f8f7319630bc66
Sakhile Mamba <sakhymamba@gmail.com>
2024-12-01 04:36:48 +0200

    Add the PUT, POST, and DELETE routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -18,4 +18,61 @@ const getSingle = async (req, res) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(user);
+  if (response.acknowledged === true) {
+    res.send(response.insertedId);
+  } else {
+    res.status(500).json({
+      message: response.error || 'Some error occurred while creating the contact.'
+    });
+  }
+};
+
+const updateContact = async (req, res) => {
+  const id = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: id }, user);
+
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json({
+      message: response.error || 'Some error occurred while creating the contact.'
+    });
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const id = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: id });
+
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json({
+      message: response.error || 'Some error occurred while deleting the contact.'
+    });
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongodb = require('./data/database');
 
@@ -7,7 +8,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
